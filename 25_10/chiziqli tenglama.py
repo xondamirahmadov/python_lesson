ebd4b8cbef6995089f79eb412f798009ae6e76fc
94036691+xondamirahmadov@users.noreply.github.com <94036691+xon03@users.noreply.github.com>
2025-10-25 13:52:43 +0500

    fff

--- 25_10/chiziqli tenglama.py
+++ 25_10/chiziqli tenglama.py
@@ -1,13 +1,18 @@
 s=input()
 k=s.find("x")
 e=s.find("=")
-a=int(s[:k])
+if k==0:
+    a=1
+elif s[:k]=='-':
+    a=-1
+else:
+    a=int(s[:k])
 b=int(s[k+1:e])
 c=int(s[e+1:])
 if a==0 and b==c:
     print("Cheksiz ko'p yechim")
 elif a==0 and b!=c:
-    print("Yechim mavjud emas")
+    print("Yechimga ega emas")
 else:
     x=(c-b)/a
     print(x)
